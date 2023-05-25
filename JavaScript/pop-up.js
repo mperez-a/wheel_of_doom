11a6487f0fc76a82b8ab468c74097b12e973d25d
rousmedina <36710468+rousmedina@users.noreply.github.com>
2023-05-25 12:38:33 -0500

    codigospopup

---
+++ JavaScript/pop-up.js
@@ -0,0 +1,18 @@
+const modalAdd = document.querySelector('#modalAdd');
+
+const openModal = () => {
+    modalAdd.style.display = 'flex';
+}
+
+
+
+const closeModal = () => {
+    modalAdd.style.display = 'none';
+}
+
+modalAdd.onclik = (event) => {
+    if(event.target == modalAdd){
+        closeModal();
+    }
+
+}
